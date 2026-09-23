0f3bf61d880ddbf94b55285c6d87db6f7245d2a2
escape <2583526642@qq.com>
2026-09-23 21:58:47 +0800

    e1-d2

---
+++ e1-d2/pilot_20260923_194508_8fljqn04/command.sh
@@ -0,0 +1,1 @@
+/home/xiexuhui/miniconda3/envs/instinctlab/bin/python -B -u scripts/instinct_rl/train.py --task Instinct-Locomotion-Flat-G1-Research-v0 --num_envs 64 --seed 42 --max_iterations 200 --headless --device cuda:0 --logroot /home/xiexuhui/InstinctLab/logs/e1-d2/pilot_20260923_194508_8fljqn04/train --run_name E1_D2_Research_s42 --e1_d2_audit
